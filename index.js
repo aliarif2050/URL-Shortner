de13c6e8103dcec4f09150c3069cc7739cf54493
Ali Arif <aliarif2050@gmail.com>
2025-08-19 15:22:46 +0500

    Added Authorization

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 const express = require('express')
-const {connectToMongoDB} = require('./connect')
+const { connectToMongoDB } = require('./connect')
 const urlRoute = require('./routes/url')
 const staticRouter = require('./routes/staticRouter')
 const userRoute = require('./routes/user')
 const path = require('path')
 const cookieParser = require('cookie-parser')
-const {restrictToLoggedInUserOnly, checkAuth} = require('./middlewares/auth')
+const { checkForAuthentication, restrictTo } = require('./middlewares/auth')
 const URL = require('./models/url')
 const app = express();
 const PORT = 8001
-connectToMongoDB('mongodb://localhost:27017/short-url').then(()=> console.log('MongoDB connected'))
-app.set("view engine","ejs")
+connectToMongoDB('mongodb://localhost:27017/short-url').then(() => console.log('MongoDB connected'))
+app.set("view engine", "ejs")
 app.set('views', path.resolve('./views'))
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use('/url',restrictToLoggedInUserOnly,urlRoute)
-app.use('/user',userRoute)
-app.use('/',checkAuth, staticRouter)
-app.get('/url/:shortId',async(req,res)=>{
+app.use(checkForAuthentication)
+app.use('/url', restrictTo(['NORMAL','Admin']), urlRoute)
+app.use('/user', userRoute)
+app.use('/', staticRouter)
+app.get('/url/:shortId', async (req, res) => {
    const shortId = req.params.shortId
 
    const entry = await URL.findOneAndUpdate({
-    shortId
-   },{$push:{
-    visitHistory:{timestamp : Date.now()} 
-   }})
+      shortId
+   }, {
+      $push: {
+         visitHistory: { timestamp: Date.now() }
+      }
+   })
    if (!entry) {
-    return res.status(404).send('Short URL not found');
-  }
+      return res.status(404).send('Short URL not found');
+   }
    res.redirect(entry.redirectURL)
 })
 
-app.listen(PORT, ()=> console.log('Server started at Port:8001'))
+app.listen(PORT, () => console.log('Server started at Port:8001'))
--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const URL = require('../models/url')
+const { restrictTo } = require('../middlewares/auth')
 const router = express.Router()
-router.get('/', async (req,res)=>{
-   if(!req.user) return res.redirect('/login')
+
+router.get('/admin/urls', restrictTo(['Admin']), async (req,res)=>{
+ 
+    const allURls = await URL.find({})
+    return res.render('home',
+      { urls:  allURls}
+    )
+})
+
+router.get('/', restrictTo(['NORMAL','Admin']), async (req,res)=>{
+ 
     const allURls = await URL.find({ createdBy : req.user._id})
     return res.render('home',
       { urls:  allURls}
